696293a7b1b21cb0fe10ef200700481f23e386b0
cesarc9091 <cesarc9091@gmail.com>
2024-07-12 21:27:50 +0000

    feat: :sparkles: Add resources (examples, tutorials, etc.)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { z, defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-const movies = defineCollection({
-	type: 'data',
+const examples = defineCollection({
+	type: 'content',
 	schema: z.object({
-		data: z.any(),
+		isTemplateLiteralToken: z.any(),
 	}),
 });
 
 // Expose your defined collection to Astro
 // with the `collections` export
 export const collections = {
-	movies,
-	docs: defineCollection({ schema: docsSchema() }),	
+	docs: defineCollection({ schema: docsSchema() }),
+	examples	
 };
---
+++ src/content/examples/example1.md
@@ -0,0 +1,5 @@
+---
+title: My Example 1
+---
+
+# Hello world!
